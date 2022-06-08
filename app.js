e2f509e85f51eb3e15c94351d22d8069e7e08c4f
Bosch <boschio91@gmail.com>
2022-06-08 18:32:29 -0400

    Files created and Giphy api implemented. GIFs load when searched, load more adds more results.

---
+++ app.js
@@ -0,0 +1,51 @@
+// GLOBAL CONSTANTS
+const API_KEY = 'nOjzgnRK5wSxGHruvjd3HVSux7Zxk46H';
+
+
+// Variables
+let gifForm = document.getElementById("form");
+let gifArea = document.querySelector(".gifs-area")
+let button = document.getElementById("search");
+let moreButton = document.getElementById("more");
+let results;
+
+let pages = 0;
+let limit = 10;
+let offset = pages * limit;
+// pages can be ++ whenever load more button is pressed
+
+gifForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    gifArea.innerHTML = ``;
+    results = document.querySelector('input').value;
+
+    pages = 0;
+    offset = pages*limit;
+    getResults(results);
+    console.log(results);
+});
+
+moreButton.addEventListener("click", () => {
+    pages += 1;
+    offset = pages*limit;
+    getResults(results);
+
+});
+
+async function getResults(event) {
+
+    // let response = await fetch(`http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${event}&limit=10&offset=0&rating=g&lang=en`);
+    let response = await fetch(`http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${event}&limit=10&offset=${offset}&rating=g&lang=en`);
+
+    let responseData = await response.json();
+    console.log(responseData)
+    responseData.data.forEach((e) => {
+        displayResults(e);
+    })
+}
+
+function displayResults(e) {
+    gifArea.innerHTML += `
+        <img id="test" src="${e.images.original.url}">
+    `;
+}
